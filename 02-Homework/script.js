8f7527c9e10f426419f14e3cdad641dd404920d2
Harbour Harrison <harb3gaucho@gmail.com>
2020-08-08 19:58:42 -0700

    added comments

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -1,3 +1,4 @@
+//HTML targeters
 var $timer = document.querySelector(".timer");
 var $button = document.querySelector(".btn");
 var $container = document.querySelector(".container");
@@ -6,6 +7,7 @@ var $title = document.querySelector('.title');
 var $innerContainer = document.querySelector(".innerContainer");
 var $highscores = document.querySelector('.highscores');
 
+// list of questions for quiz
 var questions = [
     {
         Q: "Which of the following is used to describe how elements are displayed on the screen?",
@@ -48,15 +50,17 @@ var questions = [
         answer: 'text-align: center;'
     }
 ]
+//declaring variables that will be used later on
 var secondsLeft = 60
 var i = 0
 var correctAnswerCount = 0
 var allHighscores = [];
-
+//funtion used to start the timer
 function setTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
         $timer.textContent = "Time: " + secondsLeft;
+        //if timer falls at or below 0, stop timer and end the quiz
         if (secondsLeft <= 0) {
             clearInterval(timerInterval);
             $timer.textContent = "Time: 0"
@@ -65,37 +69,37 @@ function setTime() {
         }
     }, 1000);
 }
-
+//funtion used to start quiz which clears the page, starts the clock, and asks all questions
 function askQuestion() {
     document.querySelector(".innerContainer").innerHTML = "";
     setTime();
     questionLoop();
 }
-
+//funtion used to loop through questions array and display each question on page
 function questionLoop() {
     $title.textContent = questions[i].Q;
-    // button 1
+    //answer button 1
     var btn1 = document.createElement("button");
     $innerContainer.appendChild(btn1);
     btn1.setAttribute("class", "answer-btn btn btn-success")
     btn1.textContent = questions[i].A[0];
-    // button 2
+    //answer button 2
     var btn2 = document.createElement("button");
     $innerContainer.appendChild(btn2);
     btn2.setAttribute("class", "answer-btn btn btn-success")
     btn2.textContent = questions[i].A[1];
-    // button 3
+    //answer button 3
     var btn3 = document.createElement("button");
     $innerContainer.appendChild(btn3);
     btn3.setAttribute("class", "answer-btn btn btn-success")
     btn3.textContent = questions[i].A[2];
-    // button 4
+    //answer button 4
     var btn4 = document.createElement("button");
     $innerContainer.appendChild(btn4);
     btn4.setAttribute("class", "answer-btn btn btn-success")
     btn4.textContent = questions[i].A[3];
 }
-
+//if user clicks on an answer button...
 document.querySelector(".container").addEventListener("click", function(event) {
     if (event.target.className.indexOf("answer-btn") > -1) {
         //check if the answer is right
@@ -114,11 +118,11 @@ document.querySelector(".container").addEventListener("click", function(event) {
         }
         //add count
         i++;
-        //question show
+        //show next question and answers
         questionLoop();
     }
 });
-
+//ends quiz by showing user a message and asks for user initials to save for highscore
 function endQuiz() {
     $container.innerHTML = "";
     var gameOver = document.createElement("h2");
@@ -132,15 +136,15 @@ function endQuiz() {
     $container.appendChild(saveBtn);
     saveBtn.setAttribute("class", "btn btn-success")
     saveBtn.textContent = 'save';
+    //if user clicks save, their score is saved to an array, and they are sent to highscore page
     document.querySelector(".btn").addEventListener("click", function(event) {
         if (event.target.className = 'btn-success') {
-            //log score to array function
             saveHighscore();
             highscorePage();
         }
     });
 }
-
+//takes user initials and score and adds them as an object to an array, then save to local storage
 function saveHighscore(event) {
     const $input = document.querySelector(".input");
     var initials= $input.value;
@@ -149,41 +153,45 @@ function saveHighscore(event) {
     savedHighscores();
     
 }
-
+//using local storage to either add new highscore to array or create a new item of highscores
 function savedHighscores() {
     if (localStorage.getItem("highscores")) {
         const savedScores = JSON.parse(localStorage.getItem("highscores"))
         allHighscores.push(...savedScores);
-        console.log(allHighscores);
         localStorage.setItem('highscores', JSON.stringify(allHighscores));
     } else if (!(localStorage.getItem("highscores"))) {
         localStorage.setItem('highscores', JSON.stringify(allHighscores));
     }
 }
-
+//sets page text to highscore page
 function highscorePage() {
     $container.innerHTML = "";
     var scoreboard = document.createElement("h2");
     $container.appendChild(scoreboard);
     scoreboard.textContent = "Highscores:";
+    //if a highscore exists,add ordered list of highscores to page
     if (allHighscores != null){
         var ol = document.createElement("ol");
         $container.appendChild(ol);
         ol.setAttribute("style", "width: 100px;");
+        //sort highscore array in decending order
         allHighscores.sort(function (x, y) {
             return y.scored - x.scored;
         });
+        //run through all elements in highscore array, post them to page
         for (let i = 0; i< allHighscores.length; i++) {
         var score = document.createElement("li");
         ol.appendChild(score);
         score.textContent = allHighscores[i].initials + ": " + allHighscores[i].scored;
         }
-    } 
+    }
+    //add a clear button that clears highscore array in local storage
     var clearBtn = document.createElement("button");
     $container.appendChild(clearBtn);
     clearBtn.setAttribute("class", "clearbtn btn btn-success")
     clearBtn.textContent = 'clear';
     document.querySelector(".clearbtn").addEventListener("click", function(event) {
+        //reset browser to highscore page without scores shown
         if (event.target.className = 'btn-success') {
             localStorage.clear();
             allHighscores = [];
@@ -194,14 +202,10 @@ function highscorePage() {
         }
     });
 }
-allHighscores.sort(function (x, y) {
-    return x.scored - y.scored;
-});
-
-console.log(allHighscores);
-
+//when highscore is clicked, take user to the highscore page
 $highscores.addEventListener("click", function() {
     savedHighscores();
     highscorePage(); 
 })
+//when start quiz button is clicked, ask questions
 $button.addEventListener("click", askQuestion);
